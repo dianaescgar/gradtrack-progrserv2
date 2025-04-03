a075c8e4fa938fadb1e41cbbf1c8bf5bd50d9e75
libiaf <a01253425@tec.mx>
2025-04-03 14:49:58 -0700

    Instalar e importar cors

--- index.ts
+++ index.ts
@@ -3,12 +3,14 @@ import apiRouter from './src/routes';
 import connectionDB from './src/connection/connection';
 
 const morgan = require('morgan');
+const cors = require('cors');
 
 const app: Express = express();
 const port = 3000;
 
 app.use(morgan('dev'));
 app.use(express.json()); 
+app.use(cors());
 app.use(apiRouter);
 
 connectionDB();
