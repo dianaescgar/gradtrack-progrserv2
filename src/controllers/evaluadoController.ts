edaf3616d9abb83c7bbdcd28263ba551a2157a83
dianaescg <a01253468@tec.mx>
2025-04-03 05:12:43 -0700

    Añadir controladores de evaluado para obtener por id, modificar y borrar

--- src/controllers/evaluadoController.ts
+++ src/controllers/evaluadoController.ts
@@ -57,3 +57,81 @@ export const getAllEvaluados: RequestHandler = (req: Request, res: Response) => 
       });
     });
 };
+
+export const getEvaluadoById: RequestHandler = (req: Request, res: Response) => {
+  Evaluado.findByPk(req.params.id, {
+    include: [
+      {
+        model: Poblacion,
+        include: [{
+          model: Zona
+        }],
+      },
+    ],
+  })
+    .then((data: Evaluado | null) => {
+      return res.status(200).json({
+        status: "success",
+        message: "Evaluado obtenido con éxito",
+        payload: data,
+      });
+    })
+    .catch((err) => {
+      return res.status(500).json({
+        status: "error",
+        message: "Algo ocurrió al obtener el evaluado. " + err.message,
+        payload: null,
+      });
+    });
+};
+
+
+export const modifyEvaluado: RequestHandler = (req: Request, res: Response): void => {
+if (!req.body) {
+  res.status(400).json({
+    status: "error",
+    message: "El contenido no puede estar vacío.",
+    payload: null,
+  });
+  return;  
+}
+
+
+Evaluado.update({ ...req.body }, { where: { id: req.params.id } })
+  .then(([affectedRows]) => {  
+    if (affectedRows > 0) {  
+      res.status(200).json({
+        status: "success",
+        message: "Evaluado actualizado con éxito",
+        payload: { ...req.body },
+      });    
+    } else {
+      res.status(500).json({
+        status: "error",
+        message: "Algo ocurrió al actualizar el evaluado.",
+        payload: null,
+      });
+    }
+  })
+  .catch((err) => {
+    res.status(500).json({
+      status: "error",
+      message: "Algo ocurrió al actualizar el evaluado. " + err.message,
+      payload: null,      
+    });
+  });
+};
+
+
+export const deleteEvaluado: RequestHandler = async (req: Request, res: Response): Promise<void> => {
+const { id } = req.body;
+try {
+  await Evaluado.destroy({ where: { id } });
+  res.status(200).json({ message: "Evaluado eliminado con éxito" });
+} catch (error) {
+  res.status(500).json({
+    message: "Error al eliminar el evaluado",
+    error,
+  });
+}
+};
